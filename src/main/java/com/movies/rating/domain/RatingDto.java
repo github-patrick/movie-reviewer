c0da8cd75363d61113399f8605f9f16824eed8c3
Patrick Ugwu <patrick.ugwu@yoti.com>
2019-06-07 16:39:35 +0100

    second commit

--- src/main/java/com/movies/rating/domain/RatingDto.java
+++ src/main/java/com/movies/rating/domain/RatingDto.java
@@ -1,10 +1,6 @@
 package com.movies.rating.domain;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
-import com.movies.film.domain.Film;
-import com.movies.film.domain.FilmDto;
-import com.movies.user.domain.User;
-import com.movies.user.domain.UserDto;
 import lombok.*;
 
 import java.time.LocalDateTime;
--- src/main/java/com/movies/user/domain/UserDto.java
+++ src/main/java/com/movies/user/domain/UserDto.java
@@ -1,4 +1,35 @@
 package com.movies.user.domain;
 
+import com.movies.rating.domain.RatingDto;
+import com.movies.review.domain.ReviewDto;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Setter
+@Getter
+@ToString
+@EqualsAndHashCode
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserDto {
+
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+
+    private boolean active = true;
+
+    private List<ReviewDto> reviews;
+
+    private List<RatingDto> ratings;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime modifiedAt;
+
+
+
 }
